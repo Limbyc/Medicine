3f22ac606a36a6a98438f36bca36c899008cbd44
Limbyc <124778926+Limbyc@users.noreply.github.com>
2024-04-08 15:19:14 +0300

    create translate to RegistrationFragment

--- app/src/main/java/com/valance/medicine/fragment/RegistrationFragment.kt
+++ app/src/main/java/com/valance/medicine/fragment/RegistrationFragment.kt
@@ -1,6 +1,26 @@
 package com.valance.medicine.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.valance.medicine.databinding.RegistrationFragmentBinding
 
 class RegistrationFragment : Fragment() {
+
+    private lateinit var binding: RegistrationFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = RegistrationFragmentBinding.inflate(inflater,container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
